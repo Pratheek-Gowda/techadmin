f5b9ea90cdcd065407b5dc70d7251fbe2c96516b
Pratheek Gowda <pratheek@myyahoo.com>
2026-03-21 21:51:41 +0530

    Update actions.ts

--- app/actions.ts
+++ app/actions.ts
@@ -7,7 +7,7 @@ import { revalidatePath } from 'next/cache'
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 export const prisma = globalForPrisma.prisma || new PrismaClient()
 
-if (process.env.NODE_NODE !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 // --- AUTHENTICATION ---
 
